703eeaac123559fa5fbed9cab550bee85a3239da
Aziz Ahmed <azizahmed10088@gmail.com>
2025-10-21 17:58:43 +0500

    ui

--- app/(pages)/srd/page.jsx
+++ app/(pages)/srd/page.jsx
@@ -1,10 +1,15 @@
 'use client'
+import ProgressBar from '@/components/ProgressBar';
 import { useState } from 'react';
-const Page = ({}) => {
-    const [formData, setFormData] = useState({});
+const Page = ({ }) => {
+  const [formData, setFormData] = useState({});
 
   return (
     <>
+      <div className="flex justify-center mt-8 mb-4">
+        <ProgressBar />
+        </div>
+
 
 
     </>
